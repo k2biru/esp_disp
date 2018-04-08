4985c51aab55d59d466df77058e9ebcac4f7367e
k2biru <fahrizal.hari@gmail.com>
2018-04-08 14:33:27 +0700

    Add files via upload

---
+++ ESP_DMDTest1/ESP_DMDTest1.ino
@@ -0,0 +1,113 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  App project setup:
+    Time Input widget on V1.
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "92dcc2478a4f425f80b658229f52d474";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+const char ssid[] = "Re;LieF";
+const char pass[] = "test1234";
+
+
+BLYNK_WRITE(V0)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  // You can also use:
+  // String i = param.asStr();
+  // double d = param.asDouble();
+  Serial.print("V0 Slider value is: ");
+  Serial.println(pinValue);
+}
+
+BLYNK_WRITE(V1) {
+  TimeInputParam t(param);
+
+  // Process start time
+
+  if (t.hasStartTime())
+  {
+    Serial.println(String("Start: ") +
+                   t.getStartHour() + ":" +
+                   t.getStartMinute() + ":" +
+                   t.getStartSecond());
+  }
+  // Process stop time
+
+  if (t.hasStopTime())
+  {
+    Serial.println(String("Stop: ") +
+                   t.getStopHour() + ":" +
+                   t.getStopMinute() + ":" +
+                   t.getStopSecond());
+  }
+
+  Serial.println();
+}
+
+WidgetTerminal terminal(V10);
+
+// You can send commands from Terminal to your hardware. Just use
+// the same Virtual Pin as your Terminal Widget
+BLYNK_WRITE(V10)
+{
+
+  // if you type "Marco" into Terminal Widget - it will respond: "Polo:"
+
+
+    // Send it back
+    //terminal.print("You said:");
+    //terminal.write(param.getBuffer(), param.getLength());
+    terminal.println(param.asStr());
+
+  Serial.println(param.asStr());
+
+  // Ensure everything is sent
+  terminal.flush();
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 8442);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8442);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
